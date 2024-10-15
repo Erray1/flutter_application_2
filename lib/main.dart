5645cbfbb41fe65ca88318b68bb5673185dc1ba6
Erray1 <erray837@gmail.com>
2024-10-15 10:16:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'Авторизация',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 140),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        fillColor: Color.fromARGB(255, 250, 250, 250),
+                        filled: true,
+                        labelText: 'Логин',
+                        labelStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: Color.fromARGB(255, 250, 250, 250),
+                        filled: true,
+                        labelText: 'Пароль',
+                        labelStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          // shape: RoundedRectangleBorder(
+                          //   borderRadius: BorderRadius.circular(2.0),
+                          // ),
+                          side: const BorderSide(
+                            width: 1,
+                            color: Colors.grey,
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              rememberMe = value!;
+                            });
+                          },
+                        ),
+                        Text('Запомнить меня',
+                            style: TextStyle(color: Colors.grey, fontSize: 16)),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Войти',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Регистрация',
+                          style: TextStyle(color: Colors.blue, fontSize: 16)),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          side: BorderSide(
+                            color: Colors.blue,
+                          )),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Восстановить пароль',
+                          style: TextStyle(color: Colors.grey, fontSize: 16)),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
